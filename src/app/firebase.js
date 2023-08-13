4f855ee43c22e452ac60378c4d012d3f2155f7d9
vancillo <yaredbane@gmail.com>
2023-08-12 22:42:18 -0600

    establishing connection with firebase

---
+++ src/app/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDrnQM7m2940yDp0Rr7fMQiZkSy_bi_slA',
+  authDomain: 'fir-app-a2ec9.firebaseapp.com',
+  projectId: 'fir-app-a2ec9',
+  storageBucket: 'fir-app-a2ec9.appspot.com',
+  messagingSenderId: '17733794521',
+  appId: '1:17733794521:web:4786a5f9e168656d751cc8'
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
+
