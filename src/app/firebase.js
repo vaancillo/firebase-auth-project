87816b80f2339e92dda4ebfd50f78dac3e8bb09e
vancillo <yaredbane@gmail.com>
2023-08-17 23:46:18 -0600

    commiting production

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -7,12 +7,7 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDrnQM7m2940yDp0Rr7fMQiZkSy_bi_slA',
-  authDomain: 'fir-app-a2ec9.firebaseapp.com',
-  projectId: 'fir-app-a2ec9',
-  storageBucket: 'fir-app-a2ec9.appspot.com',
-  messagingSenderId: '17733794521',
-  appId: '1:17733794521:web:4786a5f9e168656d751cc8'
+  // your config here
 }
 
 // Initialize Firebase
